efb1fec94690c63da1fc72edf69658c82a95df4a
AD <atar.dci@gmail.com>
2019-11-03 20:02:12 +0100

    Add placeholder routes

---
+++ src/components/Commemorating.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Commemorating() {
+  return <div className="App">Commemorating</div>;
+}
---
+++ src/components/Discography.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Discography() {
+  return <div className="App">Discography</div>;
+}
