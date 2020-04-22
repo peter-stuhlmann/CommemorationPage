be531cc6f278c14681bb2eb1b9002db4a9050b08
AD <atar.dci@gmail.com>
2020-04-22 19:54:22 +0200

    About page layout with placeholder

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,98 @@
 import React from 'react';
 
 export default function About() {
-  return <div className="App">About</div>;
+  return (
+    <div className="App">
+      <div class="container mt-3">
+        <div className="row">
+          <div className="col">
+            <h2 className="text-left">Biography</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img
+              src="https://picsum.photos/500/300"
+              alt="placeholder"
+              className="img-fluid float-right ml-3"
+            />
+            <p className="text-justify">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
+              culpa distinctio rerum debitis velit harum, quisquam amet,
+              provident a doloremque perferendis, eius sunt consequuntur. Totam
+              id blanditiis ipsum harum fugit. Voluptates adipisci deleniti
+              labore, beatae aut molestiae tempora omnis! Non autem cumque,
+              blanditiis optio exercitationem minus porro rerum ab delectus
+              voluptatibus consectetur iusto labore, odit, corporis ea aperiam
+              molestiae eius. Cupiditate molestias sint in quaerat quas facere
+              architecto! Debitis deleniti eligendi porro vero deserunt suscipit
+              odio perferendis, fuga at architecto nobis dignissimos quae,
+              eveniet eos? Praesentium mollitia autem facilis maiores!
+              Perspiciatis repellat maxime error libero nihil incidunt qui,
+              animi suscipit aliquam a minima eaque tempore, culpa laudantium
+              tempora at assumenda. Sequi excepturi repudiandae soluta sit velit
+              nobis est facilis praesentium? Quas facere natus sapiente esse
+              quod unde maiores hic ea dolor nulla voluptatum non magni alias
+              debitis laudantium ipsa corporis velit deleniti, optio id
+              expedita, quia cupiditate obcaecati sed! Consequuntur. Non, sunt
+              doloribus. Dignissimos ut nihil magnam quam eius, maxime cum
+              quaerat eos aliquid quod? Nisi et accusantium vitae repellat
+              cumque a vel facere optio sit corrupti. Hic, modi similique? Fugit
+              odio nobis natus itaque maiores veritatis facere magni amet
+              aspernatur magnam dolorem ducimus quidem eius cupiditate
+              praesentium saepe quam porro, corrupti ea quibusdam? Eius aut
+              magnam architecto inventore alias! Corrupti et similique iure enim
+              impedit? Inventore harum atque possimus totam cumque quam,
+              excepturi porro distinctio magni impedit enim aspernatur maxime
+              fugiat voluptates optio numquam dignissimos deserunt assumenda
+              provident sapiente. Cupiditate tenetur labore eligendi non quae
+              iure facere nihil a provident nobis, placeat alias odio laborum
+              velit soluta quo possimus unde vel repellendus quia. Deserunt
+              autem dolor dolores ratione ab! Necessitatibus optio veritatis
+              repellendus praesentium ipsa sequi! Doloribus ducimus enim nobis
+              dolore porro nulla voluptatem harum numquam quae natus atque aut
+              possimus maxime deserunt reiciendis veniam dolorum magnam, dolor
+              necessitatibus! Nobis sit, illo vel libero odit quo in quibusdam
+              vero. Velit corrupti voluptates architecto eos pariatur modi
+              similique iure itaque nobis perspiciatis, fuga, blanditiis sit
+              officiis, ducimus nisi est quasi? Eum nesciunt, magnam illo atque
+              enim accusantium fugiat, commodi quam, rem labore autem possimus
+              quis fugit alias porro eius. Id rem ipsum sed eius error tempora
+              accusamus molestias? Consectetur, quisquam! Tempora sint nulla
+              iusto est cupiditate suscipit, voluptatibus eveniet odit ratione
+              atque nostrum vero nihil. Eveniet aliquam sit qui pariatur impedit
+              dolorem voluptatibus tenetur, perferendis minima ipsum numquam
+              temporibus aspernatur. Aliquid tempora ipsum atque alias eveniet
+              rem at ratione incidunt nihil minus consectetur dolor
+              reprehenderit quisquam, tempore, nostrum cupiditate ut corporis ab
+              nobis magni nesciunt est. Repudiandae, incidunt culpa. Possimus!
+              Natus, illo maiores. Fugiat laboriosam dolorem sapiente laborum
+              adipisci expedita, aliquid nisi aliquam cumque soluta eligendi
+              libero distinctio amet dicta corrupti cupiditate rem rerum ipsa
+              illo, minus natus eum suscipit? Nesciunt odio totam maiores
+              repudiandae aspernatur omnis distinctio aperiam magnam nostrum.
+              Quia quam adipisci tempora eaque voluptatem modi iure culpa
+              aperiam nobis iste inventore, sint cupiditate eum, perferendis
+              reprehenderit soluta? Dolorem omnis assumenda laboriosam odio.
+              Laboriosam a inventore incidunt, eaque nisi asperiores architecto,
+              saepe suscipit, laudantium voluptate perferendis harum excepturi
+              nihil qui repellendus ad reprehenderit sunt rerum! Voluptas, ipsa
+              fugiat. Tempore aliquid fuga sunt! Earum sint labore culpa
+              perspiciatis perferendis velit. Porro quas veritatis vel dolorem
+              necessitatibus eos nemo, fugit nam blanditiis. Modi vel autem
+              soluta laborum aliquid esse deleniti. Debitis voluptatum velit,
+              optio, at animi mollitia commodi natus earum error alias
+              voluptatem temporibus repellendus dolorem veritatis facilis quam
+              nihil voluptates ad eos consectetur ducimus possimus fugiat eius
+              molestias? Iste. Vero esse mollitia eligendi, repudiandae
+              reiciendis laboriosam quidem illum atque perspiciatis expedita
+              molestias! Libero voluptate officia, beatae aspernatur hic at
+              veniam neque, consectetur porro voluptatem atque illo quia
+              repellendus necessitatibus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
