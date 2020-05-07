c7444db93823ec644c2211ce3e1e8535f910d578
AD <atar.dci@gmail.com>
2020-05-07 20:41:09 +0200

    Create responsive table template

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import '../css/Concerts.css';
+const data = require('../data/data.json');
 
 export default function Concerts() {
-  return <div className="App">Concerts</div>;
+  return (
+    <div className="App">
+      Concerts:
+      <table role="table" className="table">
+        <thead>
+          <tr role="row">
+            {Object.keys(data[0]).map((key) => (
+              <th role="columnheader">{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.slice(1).map((x) => (
+            <tr role="row">
+              {Object.keys(x).map((key) => (
+                <td role="cell">
+                  {Array.isArray(x[key]) ? (
+                    <ul>
+                      {x[key].map((line) => (
+                        <li>{line}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    x[key]
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
