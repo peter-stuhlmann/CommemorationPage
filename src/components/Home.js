643c94ecfd76440880584bbd1cb365209ab46108
AD <atar.dci@gmail.com>
2020-05-09 01:04:29 +0200

    Write introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
 export default function Home() {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col">
+            <h2>Hallo Tabea und Philine,</h2>
+            <p>
+              hier ein erster Entwurf der David Shallon Seite. Guckt sie an,
+              spielt ein bisschen damit rum und gebt mir euer Feedback. Im
+              Moment funktionieren die About Seite (mit Placeholder-Text und
+              Foto natürlich) und die Concerts Seite mit ein paar Einträgen.
+              Schaut euch bitte das Design auch auf dem Handy an.
+              <br />
+              LG,
+              <br />
+              Atar
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
