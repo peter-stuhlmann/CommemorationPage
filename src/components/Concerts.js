0ef57ec82c21999f04d45f20620a309acca486b0
AD <atar.dci@gmail.com>
2020-05-11 21:44:25 +0200

    Add semantic section element

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import '../css/Concerts.css';
 import YearHeading from './YearHeading';
 import ConcertsTable from './ConcertsTable';
@@ -19,13 +19,13 @@ export default function Concerts() {
         </header>
         <main>
           {data.slice(1).map((concertsByYear, index) => (
-            <Fragment key={concertsByYear.Year}>
+            <section key={concertsByYear.Year}>
               <YearHeading index={index} year={concertsByYear.Year} />
               <ConcertsTable
                 tableHeaders={tableHeaders}
                 concerts={concertsByYear.Concerts}
               />
-            </Fragment>
+            </section>
           ))}
         </main>
       </div>
