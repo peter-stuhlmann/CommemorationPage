35e7afc55e4c62ac7b59eeb51e732bd72d95abb7
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-09-21 03:00:31 +0200

    Add lazy loading to the router

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import '../css/App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
-import Home from './Home';
-import About from './About';
-import Concerts from './Concerts';
-import Discography from './Discography';
-import Media from './Media';
-import Press from './Press';
-import Gallery from './Gallery';
-import Foundation from './Foundation';
-import Commemorating from './Commemorating';
-import Legal from './Legal';
-import NotFound from './NotFound';
 import { GlobalStyles } from './GlobalStyles';
+const Home = lazy(() => import('./Home'));
+const About = lazy(() => import('./About'));
+const Concerts = lazy(() => import('./Concerts'));
+const Discography = lazy(() => import('./Discography'));
+const Media = lazy(() => import('./Media'));
+const Press = lazy(() => import('./Press'));
+const Gallery = lazy(() => import('./Gallery'));
+const Foundation = lazy(() => import('./Foundation'));
+const Commemorating = lazy(() => import('./Commemorating'));
+const Legal = lazy(() => import('./Legal'));
+const NotFound = lazy(() => import('./NotFound'));
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Navigation />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/concerts" component={Concerts} />
-        <Route exact path="/discography" component={Discography} />
-        <Route exact path="/media" component={Media} />
-        <Route exact path="/press" component={Press} />
-        <Route exact path="/gallery" component={Gallery} />
-        <Route exact path="/foundation" component={Foundation} />
-        <Route exact path="/commemorating" component={Commemorating} />
-        <Route exact path="/legal-notice" component={Legal} />
-        <Route exact path="/privacy-policy" component={Legal} />
-        <Route component={NotFound} />
-      </Switch>
+      <Suspense fallback={'Loading...'}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/concerts" component={Concerts} />
+          <Route exact path="/discography" component={Discography} />
+          <Route exact path="/media" component={Media} />
+          <Route exact path="/press" component={Press} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/foundation" component={Foundation} />
+          <Route exact path="/commemorating" component={Commemorating} />
+          <Route exact path="/legal-notice" component={Legal} />
+          <Route exact path="/privacy-policy" component={Legal} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
