6e257cabcb2ef1fe05aa2a13dbb287792dec5120
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-09-22 03:54:38 +0200

    Add photo gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PhotoGallery from 'react-photo-gallery';
+
+import { useFetch } from '../helpers/useFetch';
 
 export default function Gallery() {
-  return <div className="App">Gallery</div>;
+  const photos = useFetch('https://david-shallon-api.vercel.app/pictures');
+
+  return photos.response && <PhotoGallery photos={photos.response} />;
 }
