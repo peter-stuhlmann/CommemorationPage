90bf98aa6e10c4ab938b5ed22ae916a6c78c6d56
AD <atar.dci@gmail.com>
2020-09-27 17:17:06 +0200

    Show failed to load messgae

---
+++ src/components/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function FailedToLoad() {
+  return (
+    <div>
+      Oops! We couldn't load the requested page. Please try again in a few
+      minutes.
+    </div>
+  );
+}
