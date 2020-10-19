c91cbf3ca7e696bc2201e72e00a2548706dbbc16
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-19 17:31:15 +0200

    Create overview page for all concert-related lists

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import FlexCards from 'flex-cards';
 
 import { Heading } from './Headings';
 import { Container } from './Container';
+import { useFetch } from '../helpers/useFetch';
+import { FailedToLoad } from './Messages';
+import { screen } from '../helpers/variables';
 
 export default function Concerts() {
-  return (
-    <Container>
-      <Heading h1 title="Concerts" />
-      <p>Lorem ipsum.</p>
-    </Container>
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/cards`);
+
+  return content?.error ? (
+    <FailedToLoad />
+  ) : (
+    <Fragment>
+      <Container margin="50px auto 0 auto">
+        <Heading h1 title="Concerts" />
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </Container>
+      {content.response && (
+        <FlexCards
+          cards={content.response}
+          maxWidth={screen.desktop}
+          containerColor="transparent"
+        />
+      )}
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </Container>
+    </Fragment>
   );
 }
