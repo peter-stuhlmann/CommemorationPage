f509b884dcf5ac1502dbd2fc3d10188c36f53fad
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-19 14:59:25 +0200

    Replace <ul> with UnorderedList component

--- src/components/Orchestras.js
+++ src/components/Orchestras.js
@@ -4,6 +4,7 @@ import { useFetch } from '../helpers/useFetch';
 import { Heading } from './Headings';
 import { Container } from './Container';
 import { FailedToLoad } from './Messages';
+import { UnorderedList } from './StyledLists';
 
 export default function Orchestras() {
   const content = useFetch(`${process.env.REACT_APP_API_URL}/orchestras`);
@@ -13,11 +14,11 @@ export default function Orchestras() {
   ) : (
     <Container>
       <Heading h1 title="Orchestras" />
-      <ul>
+      <UnorderedList>
         {content?.response?.map((orchestra) => (
           <li key={orchestra.orchestra}>{orchestra.orchestra}</li>
         ))}
-      </ul>
+      </UnorderedList>
     </Container>
   );
 }
