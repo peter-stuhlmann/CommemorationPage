76b9424dc10fcd5a068488bdf2b9f40e8f4c9ff5
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-19 15:04:56 +0200

    Create Repertoire list component

---
+++ src/components/Repertoire.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { useFetch } from '../helpers/useFetch';
+import { Heading } from './Headings';
+import { Container } from './Container';
+import { FailedToLoad } from './Messages';
+import { UnorderedList } from './StyledLists';
+
+export default function Repertoire() {
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/repertoire`);
+
+  return content?.error ? (
+    <FailedToLoad />
+  ) : (
+    <Container>
+      <Heading h1 title="Repertoire" />
+      <UnorderedList>
+        {content?.response?.map((repertoire) => (
+          <li key={repertoire.piece}>{repertoire.piece}</li>
+        ))}
+      </UnorderedList>
+    </Container>
+  );
+}
