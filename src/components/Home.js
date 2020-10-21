ae25035270c6e338db1eeb3490224cf7d7128439
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-21 16:29:24 +0200

    Change home page styles (html structure in DB changed) and add flex cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,144 +1,109 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import FlexCards from 'flex-cards';
 
 import { useFetch } from '../helpers/useFetch';
-import {
-  boxShadow,
-  font,
-  link,
-  screen,
-  transition,
-} from '../helpers/variables';
+import { colors, font, screen } from '../helpers/variables';
 import { FailedToLoad } from './Messages';
 
 export default function Home() {
-  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/home`);
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/start`);
+  const cards = useFetch(`${process.env.REACT_APP_API_URL}/cards`);
 
   return content?.error ? (
     <FailedToLoad />
   ) : (
-    <StyledHome
-      dangerouslySetInnerHTML={{ __html: content?.response?.content }}
-    />
+    <Fragment>
+      <StyledHome
+        dangerouslySetInnerHTML={{ __html: content?.response?.content }}
+      />
+      {cards.response && (
+        <StyledFlexCards
+          cards={cards.response}
+          maxWidth={screen.desktop}
+          cardColor={colors.quaternary}
+        />
+      )}
+    </Fragment>
   );
 }
 
 const StyledHome = styled.div`
   .entry {
-    display: flex;
-    flex-flow: row wrap;
-    width: 100%;
-    justify-content: space-between;
-  }
-
-  .left-column {
+    background-image: url('./img/1999-david-in-birstein-1000x658.jpg');
+    background-position: center;
     background-size: cover;
     display: flex;
-    flex: 0 0 40%;
     align-items: center;
-    justify-content: center;
-    height: calc(100vh - 54px);
-    padding: 16px;
+    height: calc(100vh);
+    width: 100%;
 
-    @media (max-width: 930px) {
-      flex: 0 0 100%;
-    }
+    .entry-content {
+      color: ${font.color.quinary};
+      padding: 80px;
+      text-align: center;
 
-    img {
-      box-shadow: ${boxShadow.primary};
-      max-height: 75vh;
-      max-width: 100%;
-      border-radius: 10px;
-    }
+      h1 {
+        font-size: 90px;
+      }
 
-    .caption {
-      font-size: ${font.size.small};
-      opacity: 0.8;
+      p {
+        font-size: 40px;
+      }
     }
   }
 
-  .right-column {
-    color: #000;
-    flex: 0 0 60%;
-    height: calc(100vh - 54px);
-    padding: calc(15vh - 27px) 16px calc(15vh - 27px) 50px;
+  .two-columns {
     display: flex;
-    flex-flow: column;
-    justify-content: center;
-    align-items: flex-start;
+    justify-content: space-between;
+    flex-flow: row wrap;
 
-    @media (max-width: 930px) {
-      flex: 0 0 100%;
-      height: auto;
-      align-items: center;
+    div:first-child {
+      background-color: ${colors.senary};
+      flex: 0 0 50%;
       padding: 16px;
-    }
 
-    .entry-content {
-      display: inline-block;
-      text-align: center;
-    }
-
-    h1 {
-      font-size: 130px;
-      margin-bottom: 40px;
-
-      @media (max-width: 1650px) {
-        font-size: 100px;
-      }
-
-      @media (max-width: 1400px) {
-        font-size: 70px;
+      img {
+        width: 100%;
       }
     }
 
-    .data {
-      font-size: 50px;
-
-      @media (max-width: 1650px) {
-        font-size: 40px;
-      }
-
-      @media (max-width: 1400px) {
-        font-size: 30px;
-      }
+    div:nth-child(2) {
+      flex: 0 0 50%;
+      background-color: ${colors.senary};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-flow: column;
     }
   }
 
   .container {
-    margin: 10px auto 70px auto;
-    max-width: ${screen.desktop};
+    background-color: ${colors.tertiary};
+    width: 100%;
+    max-width: 100%;
+    color: ${font.color.senary};
+    padding: 16px;
+  }
+
+  .textblock {
+    margin: 70px 0;
+  }
+
+  .wrapper {
+    color: ${font.color.senary};
+    margin: 0 auto;
+    max-width: 1170px;
     padding: 16px;
     width: 100%;
-
-    @media (max-width: 930px) {
-      margin: 0 auto 50px 0;
-    }
-
-    h1 {
-      margin-bottom: 50px;
-    }
-
-    h2 {
-      margin: 70px 0 35px 0;
-    }
-
-    a {
-      color: ${link.primary};
-      text-decoration: none;
-      transition: ${transition.fast};
-
-      &:hover {
-        opacity: 0.7;
-      }
-    }
-
-    .info {
-      font-size: ${font.size.normal};
-      font-style: italic;
-      line-height: 1.4;
-      opacity: 0.8;
-      margin-top: 40px;
-    }
   }
+
+  .info {
+    color: ${font.color.quinary};
+    font-style: italic;
+  }
+`;
+
+const StyledFlexCards = styled(FlexCards)`
+  margin: 70px 0 30px 0;
 `;
