6b44043d2eacfd2fce00403d3794e8cbcaee58ff
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-24 22:23:52 +0200

    Change list item border color

--- src/components/StyledLists.js
+++ src/components/StyledLists.js
@@ -6,12 +6,12 @@ export const UnorderedList = styled.ul`
   padding: 0;
 
   li {
-    border-bottom: 1px solid ${colors.primary};
+    border-bottom: 1px solid ${colors.quinary};
     list-style-type: none;
     padding: 8px 0;
 
     &:first-child {
-      border-top: 1px solid ${colors.primary};
+      border-top: 1px solid ${colors.quinary};
     }
   }
 `;
