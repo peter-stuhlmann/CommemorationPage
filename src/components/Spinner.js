46c674d0a93a66305821c83639b19f4c9f75a7f8
AD <atar.dci@gmail.com>
2020-10-25 16:20:35 +0100

    cancel delay effect

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,24 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from './Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { useState } from 'react';
 
 export default function Spinner() {
-  const [showSpinner, setShowSpinner] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setShowSpinner(true);
-    }, 1000);
-    return () => {
-      setShowSpinner(false);
-    };
-  }, []);
-
   return (
-    showSpinner && (
-      <Container fixedCenter>
-        <CircularProgress />
-      </Container>
-    )
+    <Container fixedCenter>
+      <CircularProgress />
+    </Container>
   );
 }
