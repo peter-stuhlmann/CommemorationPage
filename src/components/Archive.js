4b2e80a9204ad05d235d950f39f00b50d1d00e07
AD <atar.dci@gmail.com>
2020-10-25 16:59:38 +0100

    rename Concerts to Archive

---
+++ src/components/Archive.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import '../css/Concerts.css';
+import YearTabs from './YearTabs';
+
+export default function Archive() {
+  const [years, setYears] = useState(null);
+
+  // TODO: lazyload
+  useEffect(() => {
+    const archiveYears = [];
+    for (let i = 2000; i >= 1975; i--) {
+      archiveYears.push(i);
+    }
+    setYears(archiveYears);
+  }, []);
+
+  return (
+    // TODO: add loading spinner
+    <div className="App">
+      <div className="container mt-3">
+        <header className="row">
+          <div className="col">
+            <h2 className="text-left">Concerts</h2>
+            {!years && <p>Loading...</p>}
+          </div>
+        </header>
+        <main>{years?.length && <YearTabs years={years} />}</main>
+      </div>
+    </div>
+  );
+}
