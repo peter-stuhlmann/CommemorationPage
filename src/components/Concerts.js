d782939f07f634398708f44b1c004b9db37578f9
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-10-26 15:14:33 +0100

    Fetch data from DB and remove hard coded dummy data

--- src/components/Concerts.js
+++ src/components/Concerts.js
@@ -8,45 +8,26 @@ import { FailedToLoad } from './Messages';
 import { screen } from '../helpers/variables';
 
 export default function Concerts() {
-  const content = useFetch(`${process.env.REACT_APP_API_URL}/cards`);
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/concerts`);
+  const cards = useFetch(`${process.env.REACT_APP_API_URL}/cards?tags=concerts`);
 
   return content?.error ? (
     <FailedToLoad />
   ) : (
     <Fragment>
       <Container margin="50px auto 0 auto">
-        <Heading h1 title="Concerts" />
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
+        <Heading h1 title={content.response?.title} />
+        <div dangerouslySetInnerHTML={{ __html: content.response?.content[0]}} />
       </Container>
-      {content.response && (
+      {cards.response && (
         <FlexCards
-          cards={content.response}
+          cards={cards.response}
           maxWidth={screen.desktop}
           containerColor="transparent"
         />
       )}
       <Container>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: content.response?.content[1]}} />
       </Container>
     </Fragment>
   );
