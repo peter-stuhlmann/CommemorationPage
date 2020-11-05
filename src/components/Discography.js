d1ed7026a96022313afaea4a6452ec5937b32e75
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-11-05 17:26:00 +0100

    Create overview page of all albums

--- src/components/Discography.js
+++ src/components/Discography.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import FlexCards from 'flex-cards';
+import styled from 'styled-components';
+
+import { useFetch } from '../helpers/useFetch';
+import HeaderImage from './HeaderImage';
+import { Container } from './Container';
+import { FailedToLoad } from './Messages';
 
 export default function Discography() {
-  return <div className="App">Discography</div>;
+  const albums = useFetch(`${process.env.REACT_APP_API_URL}/discography`);
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/discography`);
+
+  const albumCover = albums?.response?.map((album) => {
+    album.img.src = album.img.small;
+    album.img.alt = album.title;
+    album.href = 'discography/' + album.number.toString();
+    return album;
+  });
+
+  return albums?.error || content?.error ? (
+    <FailedToLoad />
+  ) : (
+    <Fragment>
+      <HeaderImage data={content} />
+      <Container>
+        {albums.response && <StyledFlexCards cards={albumCover} noTextbox noLabel />}
+      </Container>
+    </Fragment>
+  );
 }
+
+const StyledFlexCards = styled(FlexCards)`
+  li {
+    cursor: pointer
+  }
+`;
