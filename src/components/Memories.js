5e6e1cdcb822bdec1ead33570100770fc4579f27
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-11-16 18:55:44 +0100

    Fetch API to get content for header image

--- src/components/Memories.js
+++ src/components/Memories.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import { useFetch } from '../helpers/useFetch';
+import { Container } from './Container';
+import HeaderImage from './HeaderImage';
 
 export default function Memories() {
-  return <div className="App">Memories</div>;
+  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/memories`);
+
+  return (
+    <Fragment>
+      <HeaderImage data={content} />
+      <Container>
+        <p>Lorem ipsum.</p>
+      </Container>
+    </Fragment>
+  );
 }
