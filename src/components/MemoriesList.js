fd0b75d99a8cfc23c158613a98384031e85e1e51
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-11-17 20:25:30 +0100

    Style active list item (onClick)

--- src/components/MemoriesList.js
+++ src/components/MemoriesList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { screen } from '../helpers/variables';
+import { boxShadow, screen } from '../helpers/variables';
 
 export default function MemoriesList(props) {
-  const { content, setAuthor } = props;
-  
+  const { content, author, setAuthor } = props;
+
+  console.log(author)
   return (
     content.response 
     ? <StyledMemoriesList>
         {content.response.map((memory, index) => (
-          <li key={memory.author.name.lastName} onClick={() => setAuthor(index)}>
+          <li key={memory.author.name.lastName} onClick={() => {setAuthor(index)}} className={author === index ? 'active' : null}>
             <div>
               <img src={'/img/' + memory.author.avatar.src} alt={memory.author.avatar.alt} title={`${memory.author.name.firstName} ${memory.author.name.lastName} | (c) ${memory.author.avatar.copyright}`} />
             </div>
@@ -37,6 +38,14 @@ const StyledMemoriesList = styled.ul`
     margin-bottom: 16px;
     padding: 8px;
     text-align: center;
+
+    &.active {
+      font-weight: bold;
+      
+      img {
+        box-shadow: ${boxShadow.primary};
+      }
+    }
 
     @media (max-width: calc(${screen.tablet} - 1px)) {
       flex: 0 0 33.333%;
