f3d2d4e6847a372b1c5a51ec0f3cfeea89842e1d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-12-04 20:46:50 +0100

    Change structure to load different images on overview and detail page

--- src/components/Discography.js
+++ src/components/Discography.js
@@ -10,14 +10,26 @@ import { screen } from '../helpers/variables';
 import { meta } from '../helpers/meta';
 
 export default function Discography() {
-  const albums = useFetch(`${process.env.REACT_APP_API_URL}/discography`).response;
-  const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/discography`);
+  const albums = useFetch(`${process.env.REACT_APP_API_URL}/discography`)
+    .response;
+  const content = useFetch(
+    `${process.env.REACT_APP_API_URL}/pages/discography`
+  );
 
   const albumCover = albums?.map((album) => {
-    album.img.src = (window.innerWidth >= parseInt(screen.mobile) && window.innerWidth <= 600) || (window.innerWidth >= parseInt(screen.tablet)) ? '/img/covers/' + album.img.medium : '/img/covers/' + album.img.large;
-    album.img.alt = album.title;
-    album.href = '/discography/' + album.number.toString();
-    return album;
+    const cover = {
+      img: {
+        src:
+          (window.innerWidth >= parseInt(screen.mobile) &&
+            window.innerWidth <= 600) ||
+          window.innerWidth >= parseInt(screen.tablet)
+            ? '/img/covers/square/' + album.cover.format.square.medium
+            : '/img/covers/square/' + album.cover.format.square.large,
+        alt: album.title,
+      },
+      href: '/discography/' + album.number.toString(),
+    };
+    return cover;
   });
 
   document.title = content?.response?.meta?.title;
@@ -37,6 +49,6 @@ export default function Discography() {
 
 const StyledFlexCards = styled(FlexCards)`
   li {
-    cursor: pointer
+    cursor: pointer;
   }
 `;
