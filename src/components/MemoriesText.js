282381509688019da6ecdac158c8cd595a487174
AD <atar.dci@gmail.com>
2020-12-28 19:32:50 +0100

    change article styles

--- src/components/MemoriesText.js
+++ src/components/MemoriesText.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { screen } from '../helpers/variables';
 
 export default function MemoriesText(props) {
   const { author, content, textRef } = props;
 
   return (
     author != null && (
-      <>
-        <span ref={textRef} />
-        <StyledMemoriesText>
-          {content[author].text.map((paragraph, i) => (
-            <p
-              key={`paragraph${i}`}
-              dangerouslySetInnerHTML={{ __html: paragraph }}
-            />
-          ))}
-          <hr />
-        </StyledMemoriesText>
-      </>
+      <StyledMemoriesText ref={textRef}>
+        {content[author].text.map((paragraph, i) => (
+          <p
+            key={`paragraph${i}`}
+            dangerouslySetInnerHTML={{ __html: paragraph }}
+          />
+        ))}
+        <hr />
+      </StyledMemoriesText>
     )
   );
 }
 
 const StyledMemoriesText = styled.article`
-  margin-top: 64px;
-  margin-bottom: 48px;
+  padding-top: 32px;
+  margin: 32px 0 48px;
+
+  h2 {
+    margin-top: 0;
+  }
+
+  @media (min-width: ${screen.desktop}) {
+    padding-top: 64px;
+    margin-top: 0;
+
+    p:first-child {
+      margin-top: 32px;
+    }
+  }
 `;
