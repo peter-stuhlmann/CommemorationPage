429174305bf24490bc55f92b0c3ee6b4a1e4d763
AD <atar.dci@gmail.com>
2020-12-30 23:25:37 +0100

    Embed video in media page

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,9 +1,9 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
-import HeaderImage from './HeaderImage';
 import { Container } from './Container';
 import { useFetch } from '../helpers/useFetch';
 import { meta } from '../helpers/meta';
+import styled from 'styled-components';
 
 export default function Media() {
   const content = useFetch(`${process.env.REACT_APP_API_URL}/pages/media`);
@@ -17,11 +17,29 @@ export default function Media() {
   }, [content]);
 
   return (
-    <Fragment>
-      <HeaderImage data={content} />
-      <Container>
-        <p>Media</p>
-      </Container>
-    </Fragment>
+    <StyledContainer>
+      <iframe
+        title="Remembering David Shallon"
+        src="https://www.youtube-nocookie.com/embed/TGOqJ93AyZs"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </StyledContainer>
   );
 }
+
+const StyledContainer = styled(Container)`
+  height: 0;
+  padding-bottom: 56.25%;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    max-height: 658.125px;
+    border-radius: 4px;
+  }
+`;
